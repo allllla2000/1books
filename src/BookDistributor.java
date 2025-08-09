91d6922b6d46a4d7580dccf3f8d3290c23a6c83f
allllla2000 <intergalacticwoman@gmail.com>
2025-08-09 19:49:37 +0300

    initial commit

---
+++ src/BookDistributor.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class BookDistributor {
+
+    public static List<List<String>> distributeBooks(List<String> books) {
+
+        Collections.sort(books);
+
+        final int SHELVES_COUNT = 5;
+
+        List<List<String>> shelves = new ArrayList<>(SHELVES_COUNT);
+
+        for (int i = 0; i < SHELVES_COUNT; i++) {
+            shelves.add(new ArrayList<>());
+        }
+
+        int totalBooks = books.size();
+
+        for (int i = 0; i < totalBooks; i++) {
+            int shelfIndex = i * SHELVES_COUNT / totalBooks;
+
+            shelves.get(shelfIndex).add(books.get(i));
+        }
+        return shelves;
+    }
+
+}
+
